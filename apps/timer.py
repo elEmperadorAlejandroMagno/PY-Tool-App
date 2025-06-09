a2f485f7811e6afe95749cd45e9fff2cb4d39902
Alejandro Marvich <alejandromarvich181100@gmail.com>
2025-06-09 01:32:59 +0000

    feature: crono in real time | worst time | fix: bug messagebox best time

--- apps/timer.py
+++ apps/timer.py
@@ -51,6 +51,8 @@ class TimerApp:
         self.running = False
         self._times_list = []
         self._best_time = None
+        self._worst_time = None
+        self._average_time = None
 
         self.root = tk.Tk()
         self.root.title(self.translations['title'])
@@ -71,6 +73,9 @@ class TimerApp:
 
         self.best_time_label = tk.Label(self.root, text=f"{self.translations['best_time']}: {self.translations['no_record']}", font=("Arial", 10))
         self.best_time_label.pack(pady=10)
+
+        self.worst_time_label = tk.Label(self.root, text=f"{self.translations['worst_time']}: {self.translations['no_record']}", font=("Arial", 10))
+        self.worst_time_label.pack(pady=10)
 
         self.average_time_label = tk.Label(self.root, text=f"{self.translations['average_time']}: --", font=("Arial", 10))
         self.average_time_label.pack(pady=10)
@@ -94,6 +99,26 @@ class TimerApp:
         self._best_time = value
 
     @property
+    def worst_time(self):
+        return self._worst_time
+    
+    @worst_time.setter
+    def worst_time(self, value):
+        if value is not None and value < 0:
+            raise ValueError("El peor tiempo no puede ser negativo.")
+        self._worst_time = value
+
+    @property
+    def average_time(self):
+        return self._average_time
+    
+    @average_time.setter
+    def average_time(self, value):
+        if value is not None and value < 0:
+            raise ValueError("El tiempo promedio no puede ser negativo.")
+        self._average_time = value
+
+    @property
     def times_list(self):
         return self._times_list.copy()  # Previene modificaciones directas
 
@@ -102,20 +127,23 @@ class TimerApp:
             raise ValueError("El tiempo no puede ser negativo.")
         self._times_list.append(elapsed)
         self.best_time = min(self._times_list)
+        self.worst_time = max(self._times_list)
+        # average time deberia quitar el mejor y peor timepo, luego calcular el promedio
+        self.average_time = sum(self._times_list) / len(self._times_list) if self._times_list else None
 
     def actualizar_etiquetas(self):
         t = self.translations
         if self._times_list:
             self.times_label.config(text=f"{t['recorded_times']}: {[f'{tiempo:.2f}' for tiempo in self._times_list]}")
             average = sum(self._times_list) / len(self._times_list)
-            self.average_time_label.config(text=f"{t['average_time']}: {average:.2f} segundos")
+            self.average_time_label.config(text=f"{t['average_time']}: {average:.2f} s")
         else:
             self.times_label.config(text=f"{t['recorded_times']}: []")
             self.average_time_label.config(text=f"{t['average_time']}: --")
         if self._best_time is not None:
-            self.best_time_label.config(text=f"{t['best_time']}: {self._best_time:.2f} segundos")
-        else:
-            self.best_time_label.config(text=f"{t['best_time']}: {t['no_record']}")
+            self.best_time_label.config(text=f"{t['best_time']}: {self._best_time:.2f} s")
+        if self._worst_time is not None:
+            self.worst_time_label.config(text=f"{t['worst_time']}: {self._worst_time:.2f} s")
 
     def on_cube_change(self, *args):
         self.show_algorithm()
@@ -125,16 +153,16 @@ class TimerApp:
         if event.keysym == "space":
             if not self.running:
                 self.press_time = time.time()
-                self.label.config(text=t["press_space"])
                 self.running = True
+                self.update_timer()
             else:
                 elapsed = time.time() - self.press_time
                 self.label.config(text=f"{elapsed:.2f}")
                 self.add_time(elapsed)
-                if elapsed == self.best_time:
-                    messagebox.showinfo(t["best_time"], f"¡{t['best_time']}: {self.best_time:.2f} segundos!")
                 self.running = False
                 self.actualizar_etiquetas()
+                if elapsed == self.best_time:
+                    messagebox.showinfo(t["best_time"], f"¡{t['best_time']}: {self.best_time:.2f} segundos!")
                 # Generar nuevo algoritmo al parar el cronómetro
                 self.show_algorithm()
 
@@ -142,6 +170,12 @@ class TimerApp:
         cube_type = self.selected_cube.get().strip()
         algorithm = CubeScrambler.generate_algorithm(cube_type)
         self.result_label.config(text=f"{self.translations['generate_algorithm']}: {algorithm}")
+
+    def update_timer(self):
+        if self.running:
+            elapsed = time.time() - self.press_time
+            self.label.config(text=f"{elapsed:.2f} s")
+            self.root.after(100, self.update_timer)
 
     def run(self):
         self.root.mainloop()
